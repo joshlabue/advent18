e218824e34868c771d653311df8b2da52dafa114
Josh LaBue <joshua@labue.me>
2018-12-03 00:42:02 -0500

    Day 3
    beat bulvey on part 2 today

---
+++ 03/03-1.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var inputFile = fs.readFileSync(__dirname + '/input.txt');
+var input = inputFile.toString().split('\n');
+
+var claims = [];
+var area = [];
+
+for(i in input) {
+    claims.push({
+        id: parseInt(input[i].split(' ')[0].slice(1)),
+        x: parseInt(input[i].split(' ')[2].split(',')[0]),
+        y: parseInt(input[i].split(' ')[2].split(',')[1]),
+        width: parseInt(input[i].split(' ')[3].split('x')[0]),
+        height: parseInt(input[i].split(' ')[3].split('x')[1])
+    });
+}
+
+for(i in claims) {
+    for(var w = 0; w < claims[i].width; w++) {
+        console.log(w);
+        if(area[claims[i].x + w] == undefined) area[claims[i].x + w] = [];
+        for(var h = 0; h < claims[i].height; h++) {
+            if(area[claims[i].x + w][claims[i].y + h] == undefined) area[claims[i].x + w][claims[i].y + h] = 0;
+            area[claims[i].x + w][claims[i].y + h] += 1;
+            
+        }
+    }
+}
+
+var total = 0;
+
+for(i in area) {
+    for(j in area[i]) {
+        if(area[i][j] >= 2) total++;
+    }
+}
+
+console.log(total);
---
+++ 03/03-2.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+var inputFile = fs.readFileSync(__dirname + '/input.txt');
+var input = inputFile.toString().split('\n');
+
+var claims = [];
+var area = [];
+
+for(i in input) {
+    claims.push({
+        id: parseInt(input[i].split(' ')[0].slice(1)),
+        x: parseInt(input[i].split(' ')[2].split(',')[0]),
+        y: parseInt(input[i].split(' ')[2].split(',')[1]),
+        width: parseInt(input[i].split(' ')[3].split('x')[0]),
+        height: parseInt(input[i].split(' ')[3].split('x')[1])
+    });
+}
+
+for(i in claims) {
+    for(var w = 0; w < claims[i].width; w++) {
+        if(area[claims[i].x + w] == undefined) area[claims[i].x + w] = [];
+        for(var h = 0; h < claims[i].height; h++) {
+            if(area[claims[i].x + w][claims[i].y + h] == undefined) area[claims[i].x + w][claims[i].y + h] = [];
+            area[claims[i].x + w][claims[i].y + h].push(claims[i].id);
+        }
+    }
+}
+
+var total = 0;
+
+for(i in area) {
+    for(j in area[i]) {
+        if(area[i][j].length >= 2){
+            for(k in area[i][j]) {
+                for(c in claims) {
+                    if(claims[c].id == area[i][j][k]) claims[c].target = 'delete';
+                }
+            }
+        }
+    }
+}
+
+for(i in claims) {
+    if(claims[i].target != 'delete') console.log(claims[i]);
+}
